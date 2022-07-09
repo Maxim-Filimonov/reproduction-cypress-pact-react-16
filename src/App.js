c0a1eab5d1196c108d495a334c456827e6d66142
Maxim Filimonov <tpaktopsp@gmail.com>
2022-07-09 23:03:45 +0300

    add reproduction without cypress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pact from "@pact-foundation/pact-web";
 
 function App() {
   return (
